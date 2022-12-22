f0ae1ab39f3b2149fc85514bdeb32b38ed89e6c1
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-12-21 23:11:46 -0300

    agrega models

---
+++ src/main/java/com/portfolioRobertoMiranda/argentinaprograma/ArgentinaprogramaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioRobertoMiranda.argentinaprograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgentinaprogramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgentinaprogramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolioRobertoMiranda/argentinaprograma/ArgentinaprogramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioRobertoMiranda.argentinaprograma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgentinaprogramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
