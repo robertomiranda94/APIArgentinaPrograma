842a720950ea72b08602033292e851299c5d7bd2
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-12-30 20:37:19 -0300

    agrega funcionalidades

--- src/main/java/com/portfolioRobertoMiranda/argentinaprograma/controller/ExperienciaController.java
+++ src/main/java/com/portfolioRobertoMiranda/argentinaprograma/controller/ExperienciaController.java
@@ -1,5 +1,6 @@
 package com.portfolioRobertoMiranda.argentinaprograma.controller;
 
+
 import com.portfolioRobertoMiranda.argentinaprograma.models.Experiencia;
 import com.portfolioRobertoMiranda.argentinaprograma.services.ExperienciaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,36 +11,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/experiencia")
+@RequestMapping("/experiencia")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ExperienciaController {
-
     @Autowired
     private final ExperienciaService experienciaService;
 
     public ExperienciaController(ExperienciaService experienciaService) {
         this.experienciaService = experienciaService;
     }
-
-    @GetMapping
-    public ResponseEntity<List<Experiencia>> getExperiencia(){
+    @PutMapping("/update")
+    public ResponseEntity<Experiencia> editarExperiencia(@RequestBody Experiencia experiencia){
+        Experiencia updateExperiencia=experienciaService.editarExperiencia(experiencia);
+        return new ResponseEntity<>(updateExperiencia, HttpStatus.OK);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Experiencia>> obtenerExperiencia(){
         List<Experiencia> experiencias=experienciaService.buscarExperiencia();
         return new ResponseEntity<>(experiencias, HttpStatus.OK);
     }
-
-    @PutMapping
-    public ResponseEntity<Experiencia> editExperiencia(@RequestBody Experiencia experiencia){
-        Experiencia editExperiencia=experienciaService.editarExperiencia(experiencia);
-        return new ResponseEntity<>(editExperiencia, HttpStatus.OK);
+    @PostMapping("/add")
+    public ResponseEntity<Experiencia> crearExperiencia(@RequestBody Experiencia experiencia){
+        Experiencia nuevaExperiencia=experienciaService.addExperiencia(experiencia);
+        return new ResponseEntity<>(nuevaExperiencia, HttpStatus.CREATED);
     }
-
-    @PostMapping
-    public ResponseEntity<Experiencia> createExperiencia(@RequestBody Experiencia experiencia){
-        Experiencia newExperiencia=experienciaService.addExperiencia(experiencia);
-        return new ResponseEntity<>(newExperiencia,HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteExperiencia(@PathVariable("id") Long id){
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> borrarExperiencia(@PathVariable("id")Long id){
         experienciaService.borrarExperiencia(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
