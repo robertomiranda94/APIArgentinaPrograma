a1e6884930c6ecede6648c3a6b649551189fd08c
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-12-31 05:45:10 -0300

    persona url

--- src/main/java/com/portfolio/portfolio/Controller/PersonaController.java
+++ src/main/java/com/portfolio/portfolio/Controller/PersonaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -57,7 +57,7 @@ public class PersonaController {
         return persona;
     }
     
-    @GetMapping("personas/traer/perfil")
+    @GetMapping("/personas/traer/perfil")
     public Persona findPersona(){
         return ipersonaService.findPersona((long)1);
     }
